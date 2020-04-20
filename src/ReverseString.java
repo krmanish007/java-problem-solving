fb9bc1508e756a2a2dd376fd1ebdf4d10ac9d942
manishkumar <krmanish007@hotmail.com>
2020-04-20 22:54:11 +0100

    First commit

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -15,8 +15,7 @@ public class ReverseString {
     }
 
     public static void main(String[] args) {
-        System.out.println("reverse(\"Manish\") = " + reverse("Manish"));
-        System.out.println("reverseRecursive(\"Manish\") = " + reverseRecursive("Manish"));
-
+        System.out.println("reverse(Manish) = " + reverse("Manish"));
+        System.out.println("reverseRecursive(Manish) = " + reverseRecursive("Manish"));
     }
 }
